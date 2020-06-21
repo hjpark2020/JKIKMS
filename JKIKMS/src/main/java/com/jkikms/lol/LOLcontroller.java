23b60c22e2b363e5145d2f3538130759f4e538ac
travis.h.lee <travis.h.lee@infrawareglobal.com>
2020-06-21 13:41:30 +0900

    [LOLcontroller] match info

--- JKIKMS/src/main/java/com/jkikms/lol/LOLcontroller.java
+++ JKIKMS/src/main/java/com/jkikms/lol/LOLcontroller.java
@@ -17,14 +17,14 @@ import javax.servlet.http.HttpServletRequest;
 public class LOLcontroller {
 	@RequestMapping(value="/loltest", method=RequestMethod.GET)
 	 public JSONObject lolcontroller(Model md, HttpServletRequest request) {
-		String id="";
+		String encrypSummonerid="";
 		String nick="";
 		JSONObject jobj = null;
 	        try{
 	            String ID_LOL = "munchdrunkpurpp"; 
-	            String API_KEY_LOL = "RGAPI-9cf7d48a-87a7-4127-a7be-3a6fa36c91d6"; 
+	            String API_KEY_LOL = "RGAPI-bcd2a46e-2405-4acf-b6c0-3a579065340e"; 
 	            String urlStr = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/"+ ID_LOL +"?api_key="+API_KEY_LOL;
-	            URL url = new URL(urlStr); // 위 urlStr을 이용해서 URL 객체를 만들어줍니다. 
+	            URL url = new URL(urlStr); // 위 urlStr을 이용해서 URL 객체 생성
 	            BufferedReader bf;
 	            String line = "";
 	            String result="";
@@ -34,14 +34,26 @@ public class LOLcontroller {
 	            while((line=bf.readLine())!=null){
 	                result=result.concat(line);
 	            }
-	            //System.out.print(result);
+	            //System.out.print(result); 20200519
 	    		JSONParser parser = new JSONParser();
 	    		Object obj =parser.parse(result);
 	    		jobj = (JSONObject) obj;
 	    		//JSONObject obj = (JSONObject) parser.parse(result);
-	    		id = (String) jobj.get("id");
+	    		
+	    		System.out.println("//////////////////////////////");
+	    		encrypSummonerid= (String) jobj.get("id");
 	    		nick = (String) jobj.get("name");
-	    		//System.out.print(id);
+	    		String urlStr_entry = "https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/"+encrypSummonerid+"?api_key="+API_KEY_LOL;
+	    		URL url_entry = new URL(urlStr_entry);
+	    		String lineEn="";
+	    		String resultEn="";
+	    		InputStream ise = url_entry.openStream();
+	    		BufferedReader br = new BufferedReader(new InputStreamReader(ise));
+	    		while((lineEn=br.readLine())!=null) {
+	    			resultEn=resultEn.concat(lineEn);
+	    		}
+	    		System.out.print(resultEn);//encrypted ID 를 통한 match 정보 출력
+	    		
 	        }
 	        catch(Exception e){
 	            System.out.println(e.getMessage());
